9505a257ef3c199795210f0757c8d27ab63039ad
Zogurf <jairvillaltav08@gmail.com>
2026-04-16 21:17:43 -0500

    Instalacion de dependencias

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    )
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,16 @@
+import { Text, StyleSheet, View, Image } from 'react-native'
+import React, { useState } from 'react'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <View>
+            <View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
